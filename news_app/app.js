414b218bbe94c88cf4e09ce547e3c8bbcd87db19
G <53386620+Gnola@users.noreply.github.com>
2019-10-18 15:23:19 -0700

    Started project over due to faulty API, set up basics in news app

---
+++ news_app/app.js
@@ -0,0 +1,30 @@
+$(() => {
+
+// VARIABLES //
+const $modal = $('#modal')
+const $headlines = $('.headlines')
+const $previous = $('#previous-btn')
+const $next = $('#next-btn')
+const $headlineImgs = $('.headline-imgs')
+
+
+
+
+
+
+//   $.ajax({
+//     url: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=850ce7d10a3c44d6b8a3e6ac81eb0cb9'
+        // getting live and top breaking news headlines from US
+//   }).then( // START OF THEN //
+//   (data) => {
+//       console.log('data');
+//       // $('<div>' + data.articles[0].author + '<div>').appendTo('body')
+//       // $('<div>' + data.articles[0].title + '<div>').appendTo('body')
+//       // $('<div>' + data.articles[0].content + '<div>').appendTo('body')
+//   }, // END OF DATA //
+//   (error) => {
+//     console.log('error');
+//   } // END OF ERROR //
+// ) // END OF THEN AND AJAX //
+
+}) // END //
