20ffa2d8692d3833e45740838f66e9214459d1e5
G <53386620+Gnola@users.noreply.github.com>
2019-10-19 12:02:00 -0700

    added functionality of next button. Adding previous button

--- news_app/app.js
+++ news_app/app.js
@@ -47,22 +47,32 @@ const lastHeadline = 4;
   const promise = $.ajax({
     url: 'https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=850ce7d10a3c44d6b8a3e6ac81eb0cb9'
   }).then( // START OF THEN //
-  (data) => {
-    for (let i = 0; i < data.articles.length; i++) {
+  (data) => { // when page loads...
+    for (let i = 0; i < data.articles.length; i++) { // loop through data
       // console.log(data.articles[i].title); // gives me all 5 titles of the articles
-      titleArray.push(data.articles[i].title)
-      urlToImageArray.push(data.articles[i].urlToImage)
-    }
-    console.log(titleArray);
-    console.log(titleArray[currentHeadline]);
-    console.log(urlToImageArray);
-    console.log(urlToImageArray[currentHeadline]);
-    $next.on('click', () => {
-      currentHeadline++
-      console.log(titleArray[currentHeadline]);
-      console.log(urlToImageArray[currentHeadline]);
-    })
-  },
+      titleArray.push(data.articles[i].title) // push data titles to titleArray
+      urlToImageArray.push(data.articles[i].urlToImage) // push data urls to urlToImageArray
+    } // then...
+      $headlineImgs.css('background','url(' + urlToImageArray[currentHeadline] + ')') // append first articles url to image and set it as background
+      $headlineText.append(titleArray[currentHeadline]) // append first articles title to headline
+    // console.log(titleArray);
+    // console.log(titleArray[currentHeadline]);
+    // console.log(urlToImageArray);
+    // console.log(urlToImageArray[currentHeadline]);
+    $next.on('click', () => { // when you click the next button
+      if (currentHeadline < lastHeadline) {
+        currentHeadline++ // add to currentHeadline
+        $headlineText.empty() // empty $headlineText
+        $headlineImgs.css('background','url(' + urlToImageArray[currentHeadline] + ')') // add current article img
+        $headlineText.append(titleArray[currentHeadline]) //  add current article title
+      } else { // otherwise reset the current headline and start slideshow over
+        currentHeadline = 0;
+        $headlineText.empty()
+        $headlineImgs.css('background','url(' + urlToImageArray[currentHeadline] + ')')
+        $headlineText.append(titleArray[currentHeadline])
+      }
+    }) // END OF NEXT BUTTON FUNCTION
+  }, // END OF DATA
   (error) => {
     console.log('error');
   }
