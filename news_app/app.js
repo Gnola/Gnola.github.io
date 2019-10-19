51ae3da1134387784e0c9a5c9cafe485f7e44434
G <53386620+Gnola@users.noreply.github.com>
2019-10-19 12:30:04 -0700

    Styled close button. About to add its functionality

--- news_app/app.js
+++ news_app/app.js
@@ -20,30 +20,11 @@ let currentHeadline = 0;
 const lastHeadline = 4;
 
 
-//when you hit a certain button this will run
-//   const promise = $.ajax({ // takes 5 top headlines, adds their image and headline to headline-container
-//     url: 'https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=850ce7d10a3c44d6b8a3e6ac81eb0cb9'
-//         // getting live and top breaking news headlines from US
-//   }).then( // START OF THEN //
-//   (data) => {
-//       $headlineImgs.css('background','url(' + data.articles[currentHeadline].urlToImage + ')')
-//       $headlineText.append(data.articles[currentHeadline].title)
-//   }, // END OF DATA //
-//   (error) => {
-//     console.log('error');
-//   } // END OF ERROR //
-// ) // END OF THEN AND AJAX //
 
-// const next = (data) => {
-//   if (currentHeadline < lastHeadline) {
-//     console.log(currentHeadline);
-//     currentHeadline++
-//   } else {
-//     currentHeadline = 0;
-//   }
-// }
-//   $next.on('click', next)
 
+// hide modal until CLICKED
+// when you hit a certain button this will run
+// MODAL that takes 5 top headlines, adds their image and headline to headline-container
   const promise = $.ajax({
     url: 'https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=850ce7d10a3c44d6b8a3e6ac81eb0cb9'
   }).then( // START OF THEN //
@@ -101,8 +82,19 @@ const lastHeadline = 4;
 
 }) // END //
 
-
-
+// ORIGINAL AJAX
+//   const promise = $.ajax({ // takes 5 top headlines, adds their image and headline to headline-container
+//     url: 'https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=850ce7d10a3c44d6b8a3e6ac81eb0cb9'
+//         // getting live and top breaking news headlines from US
+//   }).then( // START OF THEN //
+//   (data) => {
+//       $headlineImgs.css('background','url(' + data.articles[currentHeadline].urlToImage + ')')
+//       $headlineText.append(data.articles[currentHeadline].title)
+//   }, // END OF DATA //
+//   (error) => {
+//     console.log('error');
+//   } // END OF ERROR //
+// ) // END OF THEN AND AJAX //
 
 
 
