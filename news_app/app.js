7b3c70fa0bd051966584958dbe081cf5699d57b6
G <53386620+Gnola@users.noreply.github.com>
2019-10-19 15:34:41 -0700

    fixed next and previous buttons. Working on first click function and adding search parameters

--- news_app/app.js
+++ news_app/app.js
@@ -3,7 +3,7 @@ $(() => {
 // VARIABLES //
   // jquery
     // main site
-const $topNews = $('#top-news')
+const $topNews = $('.top-news')
 
     // modal
 const $headlineModal = $('#headline-modal')
@@ -17,25 +17,33 @@ const $headlineModal = $('#headline-modal')
     const $close =  $('#close')
 
 
-
-
   // js
 let titleArray = [];
 let urlToImageArray = [];
 let descriptionArray = [];
+
 let currentHeadline = 0;
 const lastHeadline = 4;
-
-
-
 
 // hide modal until CLICKED
 $headlineModal.hide()
 
+
+// $("button[type='button']").on('click', (event) => {
+//   // console.log('hi');
+//     $headlineModal.show()
+//   if ($(event.currentTarget).hasClass('top-news')) {
+//     // topNews = 'top-headlines'
+//     // console.log(titleArray[currentHeadline]);
+//     // console.log(descriptionArray[currentHeadline]);
+//   }
+// })
+
 // when you hit TOP NEWS button this will run
-$topNews.on('click', () => {
-  $headlineModal.show()
-})
+// $topNews.on('click', () => {
+//   $headlineModal.show()
+//   // $headlineModal.toggle()
+// })
 // MODAL that takes 5 top headlines, adds their image and headline to headline-container
   const promise = $.ajax({
     url: 'https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=850ce7d10a3c44d6b8a3e6ac81eb0cb9'
@@ -45,31 +53,58 @@ $topNews.on('click', () => {
     for (let i = 0; i < data.articles.length; i++) { // loop through data
       titleArray.push(data.articles[i].title) // push data titles to titleArray
       urlToImageArray.push(data.articles[i].urlToImage) // push data urls to urlToImageArray
-      descriptionArray.push(data.articles[i].description)
-    } // then...
+      descriptionArray.push(data.articles[i].description) // push data description to descriptionArray
+    } // THEN WHENEVER YOU CLICK A BUTTON
+    $("button[type='button']").on('click', (event) => {
+      // if ($(event.currentTarget).hasClass('top-news')) {
+        // topNews = 'top-headlines'
+        // console.log(titleArray[currentHeadline]);
+        // console.log(descriptionArray[currentHeadline]);
+      // }
       $headlineImgs.css('background','url(' + urlToImageArray[currentHeadline] + ')') // append first articles url to image and set it as background
       $headlineText.append(titleArray[currentHeadline]) // append first articles title to headline
-      $headlineDes.append(descriptionArray[currentHeadline])
-      $headlineDes.hide()
-    // WHEN NEXT BUTTON IS CLICKED
+      $headlineDes.append(descriptionArray[currentHeadline]) // append first articles description to headlines
+      $headlineDes.hide() // hide the description
+      $headlineModal.show() // show the whole modal
+
+    }) // STILL ON LOAD
+    // $headlineImgs.css('background','url(' + urlToImageArray[currentHeadline] + ')') // append first articles url to image and set it as background
+    // $headlineText.append(titleArray[currentHeadline]) // append first articles title to headline
+    // $headlineDes.append(descriptionArray[currentHeadline]) // append first articles description to headlines
+    // $headlineDes.hide() // hide the description
+    console.log(titleArray.length);
+    console.log(descriptionArray.length);
+
+    // WHEN NEXT BUTTON IS CLICKED //
     $next.on('click', () => { // when you click the NEXT button
       if (currentHeadline < lastHeadline) {
         currentHeadline++ // add to currentHeadline
         $headlineText.empty() // empty $headlineText
-        $headlineDes.empty()
+        $headlineDes.empty() // empty $headlineDes
+
+        // FIGURE OUT NULL IMG URL BUG //
+        // if (urlToImageArray[currentHeadline] = 'null') {
+        //   console.log('hi'); // add current article img
+        // }
+
         $headlineImgs.css('background','url(' + urlToImageArray[currentHeadline] + ')') // add current article img
         $headlineText.append(titleArray[currentHeadline]) //  add current article title
-        $headlineDes.append(descriptionArray[currentHeadline])
-
+        $headlineDes.append(descriptionArray[currentHeadline]) // add current article description
+        console.log(titleArray.length);
+        console.log(descriptionArray.length);
       } else { // otherwise reset the current headline and start slideshow over
         currentHeadline = 0;
         $headlineText.empty()
         $headlineDes.empty()
         $headlineImgs.css('background','url(' + urlToImageArray[currentHeadline] + ')')
         $headlineText.append(titleArray[currentHeadline])
+        $headlineDes.append(descriptionArray[currentHeadline])
       }
     }) // END OF NEXT BUTTON FUNCTION
-    // WHEN PREVIOUS BUTTON IS CLICKED
+
+
+    // WHEN PREVIOUS BUTTON IS CLICKED //
+
     $previous.on('click', () => { // when you click the PREVIOUS button...
       if (currentHeadline > 0) {
         currentHeadline-- // add to currentHeadline
@@ -77,12 +112,18 @@ $topNews.on('click', () => {
         $headlineDes.empty()
         $headlineImgs.css('background','url(' + urlToImageArray[currentHeadline] + ')') // add current article img
         $headlineText.append(titleArray[currentHeadline]) //  add current article title
+        $headlineDes.append(descriptionArray[currentHeadline])
+        console.log(titleArray[currentHeadline]);
+        console.log(descriptionArray[currentHeadline]);
       } else { // otherwise reset the current headline and start slideshow over
         currentHeadline = lastHeadline;
         $headlineText.empty()
         $headlineDes.empty()
         $headlineImgs.css('background','url(' + urlToImageArray[currentHeadline] + ')')
         $headlineText.append(titleArray[currentHeadline])
+        $headlineDes.append(descriptionArray[currentHeadline])
+        console.log(titleArray[currentHeadline]);
+        console.log(descriptionArray[currentHeadline]);
       }
     }) // END OF PREVIOUS BUTTON FUNCTION
     // WHEN CLOSE BUTTON IS CLICKED
@@ -92,9 +133,9 @@ $topNews.on('click', () => {
     // WHEN YOU CLICK ON HEADLINE IMAGE
     $headlineImgs.on('click', () => {
       if ($headlineDes.hasClass('headline-description')) {
-        $headlineDes.toggle();
-        $headlineText.toggle();
-        $headline.toggle();
+        $headlineDes.toggle(); // toggle the description
+        $headlineText.toggle(); // toggle the headline text
+        $headline.toggle(); // toggel the headline div
       }
     }) //END OF HEADLINE IMAGE BUTTON FUNCTION
   }, // END OF DATA
