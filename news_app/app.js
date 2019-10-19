41b58c989e10cdc0bcc2d01d1ce461a3016e1fcf
G <53386620+Gnola@users.noreply.github.com>
2019-10-19 11:46:31 -0700

    set up arrays for data

--- news_app/app.js
+++ news_app/app.js
@@ -14,24 +14,25 @@ const $modal = $('#modal')
 
 
   // js
-let dataArray = []
+let titleArray = [];
+let urlToImageArray = [];
 let currentHeadline = 0;
 const lastHeadline = 4;
 
 
 //when you hit a certain button this will run
-  const promise = $.ajax({ // takes 5 top headlines, adds their image and headline to headline-container
-    url: 'https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=850ce7d10a3c44d6b8a3e6ac81eb0cb9'
-        // getting live and top breaking news headlines from US
-  }).then( // START OF THEN //
-  (data) => {
-      $headlineImgs.css('background','url(' + data.articles[currentHeadline].urlToImage + ')')
-      $headlineText.append(data.articles[currentHeadline].title)
-  }, // END OF DATA //
-  (error) => {
-    console.log('error');
-  } // END OF ERROR //
-) // END OF THEN AND AJAX //
+//   const promise = $.ajax({ // takes 5 top headlines, adds their image and headline to headline-container
+//     url: 'https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=850ce7d10a3c44d6b8a3e6ac81eb0cb9'
+//         // getting live and top breaking news headlines from US
+//   }).then( // START OF THEN //
+//   (data) => {
+//       $headlineImgs.css('background','url(' + data.articles[currentHeadline].urlToImage + ')')
+//       $headlineText.append(data.articles[currentHeadline].title)
+//   }, // END OF DATA //
+//   (error) => {
+//     console.log('error');
+//   } // END OF ERROR //
+// ) // END OF THEN AND AJAX //
 
 // const next = (data) => {
 //   if (currentHeadline < lastHeadline) {
@@ -43,18 +44,29 @@ const lastHeadline = 4;
 // }
 //   $next.on('click', next)
 
-//   const promise = $.ajax({
-//     url: 'https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=850ce7d10a3c44d6b8a3e6ac81eb0cb9'
-//   }).then( // START OF THEN //
-//   (data) => {
-//     for (let i = 0; i < data.articles.length; i++) {
-//       console.log(data.articles[i].title); // gives me all 5 titles of the articles
-//     }
-//   },
-//   (error) => {
-//     console.log('error');
-//   }
-// )
+  const promise = $.ajax({
+    url: 'https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=850ce7d10a3c44d6b8a3e6ac81eb0cb9'
+  }).then( // START OF THEN //
+  (data) => {
+    for (let i = 0; i < data.articles.length; i++) {
+      // console.log(data.articles[i].title); // gives me all 5 titles of the articles
+      titleArray.push(data.articles[i].title)
+      urlToImageArray.push(data.articles[i].urlToImage)
+    }
+    console.log(titleArray);
+    console.log(titleArray[currentHeadline]);
+    console.log(urlToImageArray);
+    console.log(urlToImageArray[currentHeadline]);
+    $next.on('click', () => {
+      currentHeadline++
+      console.log(titleArray[currentHeadline]);
+      console.log(urlToImageArray[currentHeadline]);
+    })
+  },
+  (error) => {
+    console.log('error');
+  }
+)
 
 
 
